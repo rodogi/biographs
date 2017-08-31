f09dbba25a7caacca975312ce3dcf4a02b3f2434
rodogi <rodgdor@gmail.com>
2017-08-31 21:18:22 +0200

    DOCS

--- biographs/lib/bgraph.py
+++ biographs/lib/bgraph.py
@@ -3,6 +3,19 @@ from biographs.lib.bpdb import residue_adjacency
 
 
 def network(model, cutoff=5, weight=True):
+    """Return the interaction network of a protein structure.
+
+    The interaction network is defined by a distance cutoff.
+
+    Parameters
+    ----------
+    model: Bio.PDB.model
+        The protein structure.
+    cutoff: float
+        The distance cutoff defining an interaction between two nodes.
+    weight: boolean
+        True if atomic interactions are to be considered.
+    """
 
     adjacency_dictionary = residue_adjacency(model, cutoff=cutoff,
                                             weight=weight)
