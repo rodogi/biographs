2038047cc4269fc7adc326cd92d04b37b475c624
rodogi <rodgdor@gmail.com>
2019-12-30 18:27:20 -0600

    Add two new methods to *Pmolecule*.

--- biographs/classes/pmolecule.py
+++ biographs/classes/pmolecule.py
@@ -1,5 +1,5 @@
-# class to deal with protein structures
-# python 2
+#! /usr/bin/env python3
+"""Pmolecule class is the main object to deal with protein contact networks."""
 
 from __future__ import absolute_import
 from biographs.lib.bpdb import pdb_model
@@ -33,8 +33,35 @@ class Pmolecule(object):
     """
 
     def __init__(self, structure_file, water=False):
+
         self.model = pdb_model(structure_file, water=water)
         self.path_to_file = structure_file
+
+    def __len__(self):
+        """Returns number of residues in the molecule"""
+
+        return len(list(self.model.get_residues()))
+
+    def residue(self, node):
+        """Return :Bio:PDB:Residue: from node of network
+
+        node: str, node of the form :chain:position:
+        """
+
+        if type(node) is not str:
+            raise Exception(
+                "{} should be a string of the form "
+                ":chain:position:".format(node))
+        try:
+            res = self.model[node[0]][int(node[1:])]
+        except KeyError:
+            raise Exception("node {} is not a residue in molecule".format(
+                node))
+
+        chain = res.parent
+        pos = res.id[1]
+
+        return self.model[chain.id][pos]
 
     def network(self, cutoff=5, weight=True):
         """Return the interaction network of a protein structure.
