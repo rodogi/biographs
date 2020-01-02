65b75a7c32a873d4bc160e064c04f4b0fa55d3b7
rodogi <rodgdor@gmail.com>
2020-01-02 13:29:01 -0600

    Add Licence and setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biographs",
+    version="0.1",
+    author="Rodrigo Dorantes-Gilardi",
+    author_email="rodgdor@gmail.com",
+    description="A package to work with protein structure networks",
+    url="https://github.com/rodogi/biographs",
+    keywords=["protein contact network", "protein graphs"],
+    long_description=long_description,
+    python_requires='>=3.5')
